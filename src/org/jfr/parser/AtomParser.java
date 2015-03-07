30d52ef89048b1ea3668e1e631f08be5faffe67a
Ian Aldrighetti <ianaldrighetti@outlook.com>
2015-03-07 14:20:15 -0800

    The title, content and summary tag now properly support the type attribute (issue #6).

--- src/org/jfr/parser/AtomParser.java
+++ src/org/jfr/parser/AtomParser.java
@@ -1,11 +1,19 @@
 package org.jfr.parser;
 
+import java.io.StringWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.jfr.data.Author;
 import org.jfr.data.AuthorType;
 import org.jfr.data.Feed;
 import org.jfr.data.FeedItem;
 import org.jfr.exception.ParserException;
-import org.jfr.parser.FeedParserIF;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
@@ -105,9 +113,39 @@ public class AtomParser implements FeedParserIF
 	 */
 	private String getNodeValue(Node node)
 	{
-		Node textNode = node.getChildNodes().item(0);
+		return getNodeValue(node, false);
+	}
+	
+	/**
+	 * Returns the text value of the node. This will also take into consideration the type
+	 * attribute for elements that it is applicable as well.
+	 * 
+	 * @param node
+	 * @param allowXhtml
+	 * @return
+	 */
+	private String getNodeValue(Node node, boolean allowXhtml)
+	{
+		NamedNodeMap attr = node.getAttributes();
 		
-		return textNode != null ? textNode.getNodeValue() : "";
+		// If there is no type attribute or if the type attribute starts with text or is html,
+		// there isn't anything we need to do.
+		if (attr.getNamedItem("type") == null || attr.getNamedItem("type").getNodeValue().toLowerCase().startsWith("text")
+				|| !allowXhtml || attr.getNamedItem("type").getNodeValue().equalsIgnoreCase("html"))
+		{
+			Node textNode = node.getChildNodes().item(0);
+			
+			return textNode != null ? textNode.getNodeValue() : "";
+		}
+		
+		// It should be XHTML or end in +xml or /xml.
+		String type = attr.getNamedItem("type").getNodeValue();
+		if (!type.equalsIgnoreCase("xhtml") && !type.toLowerCase().endsWith("+xml") && !type.toLowerCase().endsWith("/xml"))
+		{
+			throw new ParserException("The parser could not handle a tag (" + node.getNodeName() + ") with a type of: " + type);
+		}
+		
+		return StringEscapeUtils.escapeHtml4(convertNodeToString(node));
 	}
 	
 	/**
@@ -170,7 +208,7 @@ public class AtomParser implements FeedParserIF
 		switch (node.getNodeName().toLowerCase())
 		{
 			case "title":
-				feedItem.setTitle(getNodeValue(node));
+				feedItem.setTitle(getNodeValue(node, true));
 				break;
 				
 			case "link":
@@ -197,11 +235,11 @@ public class AtomParser implements FeedParserIF
 				
 			// TODO This is bad... possibly overwriting one or the other.
 			case "summary":
-				feedItem.setDescription(getNodeValue(node));
+				feedItem.setDescription(getNodeValue(node, true));
 				break;
 				
 			case "content":
-				feedItem.setDescription(getNodeValue(node));
+				feedItem.setDescription(getNodeValue(node, true));
 				break;
 				
 			case "author":
@@ -255,6 +293,42 @@ public class AtomParser implements FeedParserIF
 			case "uri":
 				author.setUrl(getNodeValue(node));
 				break;
+		}
+	}
+	
+	/**
+	 * Converts the contents of a node to a string (this excludes the node itself).
+	 * 
+	 * @param node
+	 * @param isXhtml
+	 * @return
+	 */
+	private String convertNodeToString(Node node)
+	{
+		// We don't want to include the node itself.
+		NodeList childNodes = node.getChildNodes();
+		
+		StringWriter writer = new StringWriter();
+		
+		try
+		{
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			
+			for (int index = 0; index < childNodes.getLength(); index++)
+			{
+				Node childNode = childNodes.item(index);
+				
+				transformer.transform(new DOMSource(childNode), new StreamResult(writer));
+			}
+			
+			return writer.toString().trim();
+		}
+		catch (Exception e)
+		{
+			throw new ParserException("An error occurred while converting an XML node to a string.", e);
 		}
 	}
 }
